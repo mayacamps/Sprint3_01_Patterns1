ad62fd3eb60a0ceb14b9de31581a4a9ae5a9f19f
Maya <maya.camps@estudiantat.upc.edu>
2023-12-17 23:23:10 +0100

    finished n1

---
+++ src/main/java/n1ex01/Main.java
@@ -0,0 +1,49 @@
+package n1ex01;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    static Scanner entry = new Scanner(System.in);
+    static Undo undo = Undo.INSTANCE;
+
+    public static void main(String[] args) {
+        boolean exit = false;
+        do {
+            switch (menu()) {
+                case 1 -> undo.addCommand(entry);
+                case 2 -> undo.deleteCommand(entry);
+                case 3 -> undo.listLastCommands(entry);
+                case 0 -> {
+                    exit = true;
+                    System.out.println("Bye!");
+                }
+                default -> System.err.println("Please introduce a valid value.\n");
+            }
+        } while (!exit);
+    }
+
+    private static int menu() {
+       return readInt("Command option menu:" +
+               "\n1. Add command" +
+               "\n2. Delete command" +
+               "\n3. List last commands" +
+               "\n0. Exit");
+    }
+
+    private static int readInt(String message){
+        while (true){
+            System.out.println(message);
+            int i;
+            try {
+                i = entry.nextInt();
+                entry.nextLine();
+                return i;
+            }
+            catch(InputMismatchException ex) {
+                System.out.println("Wrong format input. Please introduce a valid value.\n");
+                entry.nextLine();
+            }
+        }
+    }
+}
---
+++ src/main/java/n1ex01/Undo.java
@@ -0,0 +1,82 @@
+package n1ex01;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public enum Undo {
+    INSTANCE;
+    ArrayList<String> commands;
+
+    Undo(){
+        commands = new ArrayList<>();
+    }
+
+    public void addCommand(Scanner entry){
+        System.out.println("Introduce the command you want to add: ");
+        String command = entry.nextLine();
+        commands.add(command);
+        System.out.println("Command " + command + " successfully added.\n");
+    }
+
+    public void deleteCommand(Scanner entry){
+        if (commands.isEmpty()) {
+            System.out.println("Can't delete any command. Command history is empty.\n");
+            return;
+        }
+
+        listAllCommands();
+        System.out.println("Introduce the command you want to delete: ");
+        String command = entry.nextLine();
+
+        if (findCommand(command) != -1){
+            commands.remove(command);
+            System.out.println("Command " + command + " successfully deleted.\n");
+        } else {
+            System.out.println(command + " is not in command history\n");
+        }
+    }
+
+    public int findCommand(String command){
+        if (!commands.isEmpty()){
+            for (int i = 0; i < commands.size(); i++){
+                if (commands.get(i).equalsIgnoreCase(command)){
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public void listAllCommands(){
+        if (!commands.isEmpty()){
+            System.out.println("Commands list: ");
+            IntStream.range(0, commands.size()).forEach(i -> System.out.println("Command " + (i + 1) + ": " + commands.get(i)));
+        }
+    }
+
+    public void listLastCommands(Scanner entry) throws InputMismatchException {
+        if (commands.isEmpty()) {
+            System.out.println("Can't list commands. Command history is empty.\n");
+            return;
+        }
+
+        int quantCommands = 0;
+        do {
+            System.out.println("How many last commands do you want to see?");
+
+            quantCommands = entry.nextInt();
+            if (quantCommands > commands.size()){
+                System.out.println("You cannot retrieve " + quantCommands + " commands. There are " + commands.size() + " commands.\n");
+            } else if (quantCommands < 0){
+                System.out.println("Please introduce a valid value.\n");
+            }
+
+        } while (quantCommands == -1 | quantCommands > commands.size());
+
+        int startInd = commands.size() - quantCommands;
+
+        IntStream.range(startInd, commands.size()).forEach(i -> System.out.println("Command " + (i + 1) + ": " + commands.get(i)));
+    }
+}
